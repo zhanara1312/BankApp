5f8a2a14547edfe4116ce63e9e50e76351bb514b
Zhanara Ulan kyzy <Janarochka.juk@gmail.com>
2019-03-14 18:39:44 -0700

    Initial Commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// Account that represents 
+    /// Bank account. Here you can withdraw or deposit your money.
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique number for the account.
+        /// </summary>
+        public int  AccountNumber { get; set; }
+  
+        /// <summary>
+        /// Email address of the account holder.
+        /// </summary>
+
+        public String EmailAddress { get; set; }
+
+        public Decimal Balance { get; set; }
+
+        public String AccountType { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into your account. 
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        
+        public void Deposit (decimal amount)
+        {
+            // Balance = Balance + amount; 
+            Balance += amount;
+        }
+        public void Withdraw (decimal amount)
+        {
+            Balance -= amount; 
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Account();
+            a1.AccountNumber = 123456;
+            a1.Balance = 100000; 
+        }
+    }
+}
