a7540b6deaf66e26de42391ac73f5af885fb2621
Zhanara Ulan kyzy <Janarochka.juk@gmail.com>
2019-04-01 19:08:06 -0700

    Ienumerable, LINQ, Lambda and menu

--- BankApp/Bank.cs
+++ BankApp/Bank.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BankApp
 {
     static class Bank
     {
-        
-  
+        private static List<Account> accounts = new List<Account>();
+
+
         public static Account CreateAccount
             (string emailAddress, AccountType accountType,
             decimal initialDeposit)
@@ -22,9 +24,42 @@ namespace BankApp
             {
                 a1.Deposit(initialDeposit);
             }
+            accounts.Add(a1);
             return a1;
+        }
+
+        public static IEnumerable<Account> GetAllAccountsForUser()
+        {
+            return accounts;
+        }
+        private static Account GetAccountByAccountNumber
+            (int accountNumber)
+        {
+            var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+            if (account == null)
+            {
+                //throw exception
+                return null;
+            }
+            return account;
+        }
+        public static void Deposit(int accountNumber, decimal amount)
+        {
+            var account = GetAccountByAccountNumber(accountNumber);          
+            account.Deposit(amount);
+        }
+        
+        public static void Withdraw(int accountNumber, decimal amount)
+        {
+           var account = GetAccountByAccountNumber(accountNumber);
+           account.Withdraw(amount);
+        }
+
     }
 }
 
-    }
+
+
+
+    
 
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -44,13 +44,38 @@ namespace BankApp
                         Console.WriteLine($"AN: {a1.AccountNumber}, CD: {a1.CreatedDate}, Balance: {a1.Balance:C}, EA: {a1.EmailAddress}, AT: {a1.AccountType}");
                         break;
                     case "2":
+                        PrintAllAccounts();
+                        Console.Write("AccountNumber: ");
+                        var accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Amount to deposit: ");
+                        var DepositAmount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Deposit(accountNumber, DepositAmount);
+                        Console.WriteLine("Deposit successfully completed!");
+                        break;
                     case "3":
+                        Console.Write("AccountNumber: ");
+                        accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Amount to withdraw: ");
+                        var withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Withdraw(accountNumber, withdrawAmount);
+                        Console.WriteLine("Withdrawal successfully completed!");
+                        break;
                     case "4":
+                        PrintAllAccounts();
+                        break;
                     default:
                         break;
                 }
             }
         }
+        private static void PrintAllAccounts()
+        {
+            var accounts = Bank.GetAllAccountsForUser();
+            foreach (var account in accounts)
+            {
+                Console.WriteLine($"AN: {account.AccountNumber}, CD: {account.CreatedDate}, Balance: {account.Balance:C}, EA: {account.EmailAddress}, AT: {account.AccountType}");
+            }
+
+        }
     }
 }
-
